18e760d5fd860a478fd7960ed0a7183e03291e61
Mohammad Piyarji <136907810+Maddy9786@users.noreply.github.com>
2025-07-24 11:02:27 +0530

    Update addition.py

--- .src/addition.py
+++ .src/addition.py
@@ -1,9 +1,8 @@
-def add(a , b):
-return a + b
+# app.py
+# This is a test commit 123
+def add(a, b):
+    return a + b
 
 def test_add():
-   assert add(1, 3) == 4
-   assert add(3, 5) == 8 
-
-
-  
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
